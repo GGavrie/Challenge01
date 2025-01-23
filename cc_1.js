d61d2601a36558de0b5faa1ad54b21c32cb820bc
Gavrie Grant <gavrie@usf.edu>
2025-01-23 16:50:23 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+//Task 1 - Employee Information
+let employeeName = "Jason Calliste";
+const employeeID = 2900; 
+var isActive = true;
+
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
