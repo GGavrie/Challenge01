618a8e085bc7399c7c806a2c64398be9e7ff1e34
Gavrie Grant <gavrie@usf.edu>
2025-01-23 17:02:55 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,11 @@ var isActive = true;
 console.log(employeeName, typeof employeeName);
 console.log(employeeID, typeof employeeID);
 console.log(isActive, typeof isActive);
+
+// Task 2 Product Details.
+let productName = "Coffee Maker";
+const productPrice = 150;
+var isAvailable = true;
+console.log("Product Name:", productName, "Type:", typeof productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable);
